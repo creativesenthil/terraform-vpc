f39bc7abe17d9511f3b2e10aea89138ad188688e
creativesenthil <154957462+creativesenthil@users.noreply.github.com>
2025-01-13 19:50:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,8 @@
-# Define the AWS provider
-provider "aws" {
-  region = "us-east-1" # Change to your preferred region
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-# Define variables
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  description = "The name of the key pair to use for the instance"
-}
-
-# Resource to create an EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (in us-east-1)
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
-
-# Output the public IP of the instance
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+ tags = {
+   Name = var.name_tag,
+ }
 }
